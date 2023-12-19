0b4220bd65a240d31633e0cd812a6c0b5d4477bc
deadshot002 <95534508+deadshot002@users.noreply.github.com>
2023-12-19 17:15:31 +0530

    commit-2

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ function Footer(props) {
     <footer className="text-gray-600 body-font" style={props.mode}>
   <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
     <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2020 Tailblocks —
-      <a href="https://twitter.com/knyttneve" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@knyttneve</a>
+      <a href="#" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@lokeshjangir</a>
     </p>
     <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
       <a className="text-gray-500">
